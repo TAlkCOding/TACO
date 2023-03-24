e680e6be863eeb4133f82bd9bfd296aaaeb1d2ee
yhw0104 <yhw0104@naver.com>
2023-03-24 17:37:05 +0900

    init commit

---
+++ src/main/java/sideproject/talkcoding/TalkcodingApplication.java
@@ -0,0 +1,13 @@
+package sideproject.talkcoding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalkcodingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TalkcodingApplication.class, args);
+	}
+
+}

4611d51910685f5fb0c01f028e33ec0d92d1c7bc
yhw0104 <yhw0104@naver.com>
2023-04-04 20:18:30 +0900

    add postEntity column

--- src/main/java/sideproject/talkcoding/model/entity/post/PostEntity.java
+++ src/main/java/sideproject/talkcoding/model/entity/post/PostEntity.java
@@ -1,11 +1,15 @@
 package sideproject.talkcoding.model.entity.post;
 
+import java.time.LocalDateTime;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+
+import org.hibernate.annotations.CreationTimestamp;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -24,9 +28,24 @@ public class PostEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column
+    @Column(nullable = false)
     private String title;
 
     @Column
     private String description;
+
+    @CreationTimestamp //INSERT 쿼리가 발생할 때, 현재 시간을 값으로 채워서 쿼리를 생성한다.
+    @Column
+    private LocalDateTime postRegDate;
+
+    @Column
+    private String postSido;
+
+    @Column
+    private String postGugun;
+
+    @Column
+    private String postDong;
+
+    
 }

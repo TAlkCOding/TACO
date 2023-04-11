dac78250009359ecab2093dd75228c2044faf067
yhw0104 <yhw0104@naver.com>
2023-04-11 21:19:24 +0900

    today last commit

--- src/main/java/sideproject/talkcoding/service/post/PostService.java
+++ src/main/java/sideproject/talkcoding/service/post/PostService.java
@@ -1,5 +1,6 @@
 package sideproject.talkcoding.service.post;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,6 +30,13 @@ public class PostService {
 
         return post;
 	}
+
+    // 게시글 리스트 가져오기
+    public List<PostEntity> readAll(){
+        List<PostEntity> list = postRepository.findAll();
+
+        return list;
+    }
 
 	public void delete(Long postId) {
         postRepository.deleteById(postId);

dace0f840a2c0c18dbad5f9c8e82fdba60aa52db
yhw0104 <yhw0104@naver.com>
2023-06-07 00:14:54 +0900

    search method

--- src/main/java/sideproject/talkcoding/repository/PostRepository.java
+++ src/main/java/sideproject/talkcoding/repository/PostRepository.java
@@ -17,4 +17,6 @@ public interface PostRepository extends JpaRepository<PostEntity, Long>{
     List<PostEntity> findByPostSidoAndPostGugunAndPostDong(String postSido, String postGugun, String postDong);
 
     List<PostEntity> findByUserIndex(Long userIndex);
+
+    List<PostEntity> findByTitleContaining(String keyword);
 }
--- src/main/java/sideproject/talkcoding/service/post/PostService.java
+++ src/main/java/sideproject/talkcoding/service/post/PostService.java
@@ -1,5 +1,6 @@
 package sideproject.talkcoding.service.post;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -92,4 +93,18 @@ public class PostService {
         
         return dongList;
     }
+
+    // 게시글 검색
+    public List<PostEntity> search(String keyword) {
+
+        List<PostEntity> search = postRepository.findByTitleContaining(keyword);
+        List<PostEntity> searchList = new ArrayList<>();
+
+        if(search.isEmpty()) return search;
+
+        for(PostEntity post : search) {
+            searchList.add(post);
+        }
+        return searchList;
+    }   
 }
